e12062cc56c1020fcaa05b3795f163626ebc4136
dilalodi <dilalodi@hotmail.com>
2020-09-01 21:30:29 -0500

    reinitialized files

---
+++ javascript/demo.js
@@ -0,0 +1,11 @@
+alert("Mi primer ventana de alerta!");
+
+
+var myNum1 = 7;
+var myNum2 = 10;
+if (myNum1 > myNum2) {
+   alert("JavaScript is easy to learn.");
+}
+
+var x=38%5;
+document.write(x);
